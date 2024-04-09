b4f7e38e8e4af62f121cdd0709fbd46974f20606
dongseoki <43495319+dongseoki@users.noreply.github.com>
2024-04-10 01:01:44 +0900

    [test] value, variable test

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,4 +3,8 @@
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
 G
-src/main/kotlin/Main.kt,d/8/d83ae3847b7d408136086cc5e9e95f5f7c126ed5
+src/main/kotlin/Main.kt,d\8\d83ae3847b7d408136086cc5e9e95f5f7c126ed5
+>
+ktExercise.iml,3\8\38ac42d852a58945b242bf36fd4a328f68555fd8
+G
+src/main/kotlin/Test.kt,8\0\80a8889682cb507169805d7cc1f735335d357797
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
+import Test
 fun main(args: Array<String>) {
     println("Hello World!")
-
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    val a= 10
+    println(a)
+    var test = "ASDG";
+    println(test)
+
+    val b: Int = 10
+    println(b)
+
+    val c: String = "SDFG"
+    println(c)
 }
